41b6afb715622dea4fa606797e4a950226dc3ba1
Peppe2496 <giuseppemusellajob@gmail.com>
2023-11-27 11:14:00 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function typeOfService(event) {
+    event.preventDefault();
+    let frontEndService = document.getElementById("frontEnd").value
+    let backEndService = document.getElementById("backEnd").value
+    let projectAnalystService = document.getElementById("projectAnalyst").value
+    let hoursRequested = document.getElementById("hours").value
+    hoursRequested = parseInt("hoursRequested");
+}
