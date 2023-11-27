15e6420e65c6e6ee013cf8794440789edc641f1d
Peppe2496 <giuseppemusellajob@gmail.com>
2023-11-27 13:00:18 +0100

    ended

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+let discountCodes ={
+    YHDNU32: true,
+    JANJC63: true,
+    PWKCN25: true,
+    SJDPO96: true,
+    POCIE24: true,
+};
+
+let priceOfTypeOfWorks ={
+    backend: 20.50,
+    frontend: 15.30,
+    projectAnalyst: 33.60,
+};
+
+
+let formattatorePrezzo = new Intl.NumberFormat(navigator.languages[0],{
+    style:"currency",
+    currency: "EUR"
+});
+
+let formElement = document.getElementById("validForm");
+let typeOfWork = document.getElementById("typeOfWork");
+let hoursRequested = document.getElementById("hours");
+let spanPrice = document.getElementById("spanPrice");
+let discountCode = document.getElementById("discount");
+let validDiscount = document.getElementById("feedbackValidDiscount");
+let expiredDiscount = document.getElementById("feedbackExpiredDiscount");
 
 
 
@@ -6,18 +33,71 @@
 
 
 
-
-
-
-
-
-
-
-function typeOfService(event) {
+function checkForm(event) {
     event.preventDefault();
-    let frontEndService = document.getElementById("frontEnd").value
-    let backEndService = document.getElementById("backEnd").value
-    let projectAnalystService = document.getElementById("projectAnalyst").value
-    let hoursRequested = document.getElementById("hours").value
-    hoursRequested = parseInt("hoursRequested");
+
+
+
+let validForm = formElement.checkValidity();
+if(validForm) {
+    let enteredCode = discountCode.value;
+    let price = priceOfTypeOfWorks[typeOfWork.value] * hoursRequested.value;
+    if (enteredCode) {
+        if(discountCodes.hasOwnProperty(enteredCode)) {
+            if (discountCodes[enteredCode]) {
+                price = price * (1-0.25);
+                discountCodes[enteredCode] = false;
+                getValidDiscount();
+                getPrice(price);
+
+            } else {
+                getExpiredDiscount();
+                getPrice(price);
+            }
+        } 
+    } else getPrice(price);
+  }
+};
+
+
+
+
+
+
+
+
+
+function getPrice(price) {
+    spanPrice.parentNode.parentNode.classList.remove("d-none");
+    spanPrice.innerHTML = formattatorePrezzo.format(price);
 }
+
+function getValidDiscount() {
+    validDiscount.innerHTML = "Lo sconto del 25% é stato applicato";
+    
+
+}
+
+function getExpiredDiscount() {
+    expiredDiscount.innerHTML = "Codice scaduto";
+    discountCode.classList.add("is-invalid", "text-danger");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
